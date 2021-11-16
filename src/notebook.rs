1ad594c6be60a3a8863a657febf2e54ddba37f1a
Satsuki Akiba <anosatsuk124@gmail.com>
2021-11-16 11:03:19 +0900

    Initial commit

---
+++ src/notebook.rs
@@ -0,0 +1,17 @@
+// ---
+// jupyter:
+//   jupytext:
+//     text_representation:
+//       extension: .rs
+//       format_name: light
+//       format_version: '1.5'
+//       jupytext_version: 1.13.1
+//   kernelspec:
+//     display_name: Rust
+//     language: rust
+//     name: rust
+// ---
+
+// # Hello world
+
+println!("Hello world!");
